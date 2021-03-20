a814ca1ccfcdbb46c2a0b30899039a5343439744
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-03-20 14:52:27 +0200

    display area styling

--- client/src/App.js
+++ client/src/App.js
@@ -7,255 +7,254 @@ import { useState } from "react";
 const axios = require("axios").default;
 
 function App() {
-  const [state, setState] = useState({
-    error: false,
-    input: "",
-    pokemon: {
-      Name: "",
-      Height: "",
-      Weight: "",
-      Types: [],
-      caught: false,
-      img: "",
-    },
-    info: { next: null, prev: null, results: [] },
+ const [state, setState] = useState({
+  error: false,
+  input: "",
+  pokemon: {
+   Name: "",
+   Height: "",
+   Weight: "",
+   Types: [],
+   caught: false,
+   img: "",
+  },
+  info: { next: null, prev: null, results: [] },
+ });
+
+ function search(e) {
+  const input = e.target.value;
+  setState({
+   error: false,
+   input,
+   pokemon: state.pokemon,
+   info: state.info,
   });
+ }
 
-  function search(e) {
-    const input = e.target.value;
+ async function toggleCatchRelease() {
+  try {
+   const collection = (await axios.get(`/api/collection`)).data.userCollection;
+
+   const data = state.pokemon.caught
+    ? await axios.delete(`/api/collection/release/${state.pokemon.name}`)
+    : await axios.post(`/api/collection/catch`, {
+       name: state.pokemon.name,
+      });
+
+   const info = data.data;
+   state.pokemon.caught = !state.pokemon.caught;
+
+   if (JSON.stringify(state.info) === JSON.stringify(collection)) {
     setState({
+     error: false,
+     input: state.input,
+     pokemon: state.pokemon,
+     info,
+    });
+    return;
+   }
+
+   setState({
+    error: false,
+    input: state.input,
+    pokemon: state.pokemon,
+    info: state.info,
+   });
+  } catch {
+   setState({
+    error: true,
+    input: state.input,
+    pokemon: state.pokemon,
+    info: state.info,
+   });
+  }
+ }
+
+ function showEveryPokemon() {
+  axios
+   .get(`/api/pokemon`)
+   .then((data) => {
+    const info = data.data;
+    setState({
+     error: false,
+     input: state.input,
+     pokemon: state.pokemon,
+     info,
+    });
+
+    getPokemonImages(info);
+   })
+   .catch(() => {
+    setState({
+     error: true,
+     input: state.input,
+     pokemon: state.pokemon,
+     info: state.info,
+    });
+   });
+ }
+
+ function showAnotherPage(nextOrPrev) {
+  if (!nextOrPrev) return;
+
+  axios
+   .get(`/api/pokemon/${nextOrPrev}`)
+   .then((data) => {
+    const info = data.data;
+    setState({
+     error: false,
+     input: state.input,
+     pokemon: state.pokemon,
+     info,
+    });
+   })
+   .catch(() => {
+    setState({
+     error: true,
+     input: state.input,
+     pokemon: state.pokemon,
+     info: state.info,
+    });
+   });
+ }
+
+ function showCollection() {
+  axios
+   .get(`/api/collection`)
+   .then((data) => {
+    const info = data.data;
+    setState({
+     error: false,
+     input: state.input,
+     pokemon: state.pokemon,
+     info,
+    });
+
+    getPokemonImages(info);
+   })
+   .catch(() => {
+    setState({
+     error: true,
+     input: state.input,
+     pokemon: state.pokemon,
+     info: state.info,
+    });
+   });
+ }
+
+ function getPokemonImages(info) {
+  info.results.map((pokemon) => {
+   axios
+    .get(`/api/pokemon/${pokemon.name}`)
+    .then(({ data }) => {
+     const { front_default } = data.img;
+     pokemon.img = front_default;
+     setState({
       error: false,
-      input,
+      input: state.input,
       pokemon: state.pokemon,
-      info: state.info,
+      info,
+     });
+    })
+    .catch(() => {
+     console.log("Some of the pokemon images couldn't load");
     });
-  }
+  });
+ }
 
-  async function toggleCatchRelease() {
-    try {
-      const collection = (await axios.get(`/api/collection`)).data
-        .userCollection;
-
-      const data = state.pokemon.caught
-        ? await axios.delete(`/api/collection/release/${state.pokemon.name}`)
-        : await axios.post(`/api/collection/catch`, {
-            name: state.pokemon.name,
-          });
-
-      const info = data.data;
-      state.pokemon.caught = !state.pokemon.caught;
-
-      if (JSON.stringify(state.info) === JSON.stringify(collection)) {
-        setState({
-          error: false,
-          input: state.input,
-          pokemon: state.pokemon,
-          info,
-        });
-        return;
-      }
-
-      setState({
-        error: false,
-        input: state.input,
-        pokemon: state.pokemon,
-        info: state.info,
-      });
-    } catch {
-      setState({
-        error: true,
-        input: state.input,
-        pokemon: state.pokemon,
-        info: state.info,
-      });
-    }
-  }
-
-  function showEveryPokemon() {
-    axios
-      .get(`/api/pokemon`)
-      .then((data) => {
-        const info = data.data;
-        setState({
-          error: false,
-          input: state.input,
-          pokemon: state.pokemon,
-          info,
-        });
-
-        getPokemonImages(info);
-      })
-      .catch(() => {
-        setState({
-          error: true,
-          input: state.input,
-          pokemon: state.pokemon,
-          info: state.info,
-        });
-      });
-  }
-
-  function showAnotherPage(nextOrPrev) {
-    if (!nextOrPrev) return;
-
-    axios
-      .get(`/api/pokemon/${nextOrPrev}`)
-      .then((data) => {
-        const info = data.data;
-        setState({
-          error: false,
-          input: state.input,
-          pokemon: state.pokemon,
-          info,
-        });
-      })
-      .catch(() => {
-        setState({
-          error: true,
-          input: state.input,
-          pokemon: state.pokemon,
-          info: state.info,
-        });
-      });
-  }
-
-  function showCollection() {
-    axios
-      .get(`/api/collection`)
-      .then((data) => {
-        const info = data.data;
-        setState({
-          error: false,
-          input: state.input,
-          pokemon: state.pokemon,
-          info,
-        });
-
-        getPokemonImages(info);
-      })
-      .catch(() => {
-        setState({
-          error: true,
-          input: state.input,
-          pokemon: state.pokemon,
-          info: state.info,
-        });
-      });
-  }
-
-  function getPokemonImages(info) {
-    info.results.map((pokemon) => {
-      axios
-        .get(`/api/pokemon/${pokemon.name}`)
-        .then(({ data }) => {
-          const { front_default } = data.img;
-          pokemon.img = front_default;
-          setState({
-            error: false,
-            input: state.input,
-            pokemon: state.pokemon,
-            info,
-          });
-        })
-        .catch(() => {
-          console.log("Some of the pokemon images couldn't load");
-        });
+ function getTypesList(e) {
+  const type = e.target.innerText;
+  axios
+   .get(`/api/type/${type}`)
+   .then((data) => {
+    const info = data.data;
+    setState({
+     error: false,
+     input: state.input,
+     pokemon: state.pokemon,
+     info,
     });
-  }
 
-  function getTypesList(e) {
-    const type = e.target.innerText;
-    axios
-      .get(`/api/type/${type}`)
-      .then((data) => {
-        const info = data.data;
-        setState({
-          error: false,
-          input: state.input,
-          pokemon: state.pokemon,
-          info,
-        });
-
-        getPokemonImages(info);
-      })
-      .catch(() => {
-        setState({
-          error: true,
-          input: state.input,
-          pokemon: state.pokemon,
-          info: state.info,
-        });
-      });
-  }
-
-  function searchPokemon() {
-    const name = state.input.toLowerCase();
-
-    if (name === "") return;
-
-    axios
-      .get(`/api/pokemon/${name}`)
-      .then((data) => {
-        const pokemon = data.data;
-        setState({
-          error: false,
-          input: state.input,
-          pokemon,
-          info: state.info,
-        });
-      })
-      .catch(() => {
-        setState({
-          error: true,
-          input: state.input,
-          pokemon: state.pokemon,
-          info: state.info,
-        });
-      });
-  }
-
-  function showPokemon(e) {
-    const name = e.target.innerText;
-    axios.get(`/api/pokemon/${name}`).then((data) => {
-      const pokemon = data.data;
-      setState({
-        error: false,
-        input: state.input,
-        pokemon,
-        info: state.info,
-      });
+    getPokemonImages(info);
+   })
+   .catch(() => {
+    setState({
+     error: true,
+     input: state.input,
+     pokemon: state.pokemon,
+     info: state.info,
     });
-  }
+   });
+ }
 
-  return (
-    <>
-      <h1>Pokedex</h1>
-      <SearchArea
-        search={search}
-        showCollection={showCollection}
-        searchPokemon={searchPokemon}
-        showEveryPokemon={showEveryPokemon}
-      />
-      {state.error ? (
-        <div>
-          <img src="./sad_pikachu.gif" />
-        </div>
-      ) : (
-        <Display
-          toggleCatchRelease={toggleCatchRelease}
-          pokemon={state.pokemon}
-          getTypesList={getTypesList}
-        />
-      )}
-      {state.info.length !== 0 && (
-        <Info
-          info={state.info}
-          showPokemon={showPokemon}
-          showAnotherPage={showAnotherPage}
-        />
-      )}
-    </>
-  );
+ function searchPokemon() {
+  const name = state.input.toLowerCase();
+
+  if (name === "") return;
+
+  axios
+   .get(`/api/pokemon/${name}`)
+   .then((data) => {
+    const pokemon = data.data;
+    setState({
+     error: false,
+     input: state.input,
+     pokemon,
+     info: state.info,
+    });
+   })
+   .catch(() => {
+    setState({
+     error: true,
+     input: state.input,
+     pokemon: state.pokemon,
+     info: state.info,
+    });
+   });
+ }
+
+ function showPokemon(e) {
+  const name = e.target.innerText;
+  axios.get(`/api/pokemon/${name}`).then((data) => {
+   const pokemon = data.data;
+   setState({
+    error: false,
+    input: state.input,
+    pokemon,
+    info: state.info,
+   });
+  });
+ }
+
+ return (
+  <>
+   <h1 className="header">Pokedex</h1>
+   <SearchArea
+    search={search}
+    showCollection={showCollection}
+    searchPokemon={searchPokemon}
+    showEveryPokemon={showEveryPokemon}
+   />
+   {state.error ? (
+    <div>
+     <img src="./sad_pikachu.gif" />
+    </div>
+   ) : (
+    <Display
+     toggleCatchRelease={toggleCatchRelease}
+     pokemon={state.pokemon}
+     getTypesList={getTypesList}
+    />
+   )}
+   {state.info.length !== 0 && (
+    <Info
+     info={state.info}
+     showPokemon={showPokemon}
+     showAnotherPage={showAnotherPage}
+    />
+   )}
+  </>
+ );
 }
 
 export default App;
--- client/src/components/SearchArea.js
+++ client/src/components/SearchArea.js
@@ -1,18 +1,24 @@
 import React from "react";
 
 export default function SearchArea({
-  search,
-  value,
-  searchPokemon,
-  showCollection,
-  showEveryPokemon,
+ search,
+ value,
+ searchPokemon,
+ showCollection,
+ showEveryPokemon,
 }) {
-  return (
-    <div>
-      <input onChange={search} value={value} required />
-      <button onClick={(e) => searchPokemon(e)}>Search Pokemon</button>
-      <button onClick={showCollection}>Show Collection</button>
-      <button onClick={showEveryPokemon}>Show All Pokemon</button>
-    </div>
-  );
+ return (
+  <div className="SearchArea">
+   <input className="input" onChange={search} value={value} required />
+   <button className="btn" onClick={(e) => searchPokemon(e)}>
+    Search Pokemon
+   </button>
+   <button className="btn" onClick={showCollection}>
+    Show Collection
+   </button>
+   <button className="btn" onClick={showEveryPokemon}>
+    Show All Pokemon
+   </button>
+  </div>
+ );
 }
