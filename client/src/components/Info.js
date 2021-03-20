225894cda8461dc1d899a5f4062e1bda38828d87
Yairderry <77237113+Yairderry@users.noreply.github.com>
2021-03-20 19:27:19 +0200

    Fixed catch release bug

--- client/src/components/Info.js
+++ client/src/components/Info.js
@@ -2,31 +2,31 @@ import React from "react";
 import Pokemon from "./Pokemon";
 
 export default function Info({ info, showPokemon, showAnotherPage }) {
- return (
-  <div className="infoDisplay">
-   {info.results.length !== 0 && (
-    <>
-     <button
-      onClick={() => {
-       showAnotherPage(info.prev);
-      }}
-     >
-      prev page
-     </button>
-     <ul>
-      {info.results.map((pokemon, i) => (
-       <Pokemon key={i} pokemon={pokemon} showPokemon={showPokemon} />
-      ))}
-     </ul>
-     <button
-      onClick={() => {
-       showAnotherPage(info.next);
-      }}
-     >
-      next page
-     </button>
-    </>
-   )}
-  </div>
- );
+  return (
+    <div className="infoDisplay">
+      {info.results.length !== 0 && (
+        <>
+          <button
+            onClick={() => {
+              showAnotherPage(info.prev);
+            }}
+          >
+            prev page
+          </button>
+          <ul>
+            {info.results.map((pokemon, i) => (
+              <Pokemon key={i} pokemon={pokemon} showPokemon={showPokemon} />
+            ))}
+          </ul>
+          <button
+            onClick={() => {
+              showAnotherPage(info.next);
+            }}
+          >
+            next page
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
