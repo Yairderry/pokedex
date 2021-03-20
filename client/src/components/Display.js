8dcfba1026aa36f558ed2ba155ddf9ae58d8cc42
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-03-20 20:55:50 +0200

    Revert "Merge branch 'main' of https://github.com/Yairderry/pokedex into main"
    This reverts commit 0346a552b6b1025c7335f015d2779caae723c37a, reversing
    changes made to 5bdf70eae3f17e39cf7e8dacd054ec1f8d1733b9.

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -2,50 +2,42 @@ import React from "react";
 import PokemonProperty from "./PokemonProperty";
 
 export default function Display({ getTypesList, pokemon, toggleCatchRelease }) {
-  function printProperties(pokemon) {
-    const PropertiesList = [];
-    for (let prop in pokemon) {
-      if (prop === "caught" || prop === "img") continue;
-      PropertiesList.push(
-        <PokemonProperty
-          key={prop}
-          prop={prop}
-          value={pokemon[prop]}
-          getTypesList={getTypesList}
-        />
-      );
-    }
-    return PropertiesList;
+ function printProperties(pokemon) {
+  const PropertiesList = [];
+  for (let prop in pokemon) {
+   if (prop === "caught" || prop === "img") continue;
+   PropertiesList.push(
+    <PokemonProperty
+     key={prop}
+     prop={prop}
+     value={pokemon[prop]}
+     getTypesList={getTypesList}
+    />
+   );
   }
+  return PropertiesList;
+ }
 
-  return (
-    <>
-      <div className="displayPokemon">
-        <ul className="properties">{printProperties(pokemon)}</ul>
-        <img
-          className="img"
-          src={pokemon.img.front_default}
-          onMouseOver={(e) => (e.currentTarget.src = pokemon.img.back_default)}
-          onMouseOut={(e) => (e.currentTarget.src = pokemon.img.front_default)}
-        />
-        {!pokemon.name ? null : pokemon.caught ? (
-          <button
-            className="btn"
-            id="catchRelease"
-            onClick={toggleCatchRelease}
-          >
-            Release
-          </button>
-        ) : (
-          <button
-            className="btn"
-            id="catchRelease"
-            onClick={toggleCatchRelease}
-          >
-            Catch
-          </button>
-        )}
-      </div>
-    </>
-  );
+ return (
+  <>
+   <div className="displayPokemon">
+    <ul className="properties">{printProperties(pokemon)}</ul>
+    <img
+     className="img"
+     src={pokemon.img.front_default}
+     onMouseOver={(e) => (e.currentTarget.src = pokemon.img.back_default)}
+     onMouseOut={(e) => (e.currentTarget.src = pokemon.img.front_default)}
+    />
+   </div>
+   {!pokemon.name ? null : pokemon.caught ? (
+    <button className="btn" id="catchRelease" onClick={toggleCatchRelease}>
+     Release
+    </button>
+   ) : (
+    <button className="btn" id="catchRelease" onClick={toggleCatchRelease}>
+     Catch
+    </button>
+   )}
+  </>
+ );
 }
