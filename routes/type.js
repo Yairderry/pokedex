084880c649eec5be8bef40bc391ad1f309302814
Yairderry <77237113+Yairderry@users.noreply.github.com>
2021-03-21 03:05:46 +0200

    mabye

--- routes/type.js
+++ routes/type.js
@@ -4,19 +4,20 @@ const { getPokemon, isPokemonCaught } = require("../utils/pokeAPI");
 const type = Router();
 
 type.get("/:type", async (req, res) => {
-  try {
-    const data = await getPokemon(req.originalUrl);
-    const results = await Promise.all(
-      data.pokemon.map(async ({ pokemon }) => ({
-        name: pokemon.name,
-        img: null,
-        caught: await isPokemonCaught(pokemon.name, req),
-      }))
-    );
-    res.json({ next: null, prev: null, results });
-  } catch {
-    res.status(404).json({ error: "No such pokemon in this collection" });
-  }
+  res.send("What");
+  // try {
+  //   const data = await getPokemon(req.originalUrl);
+  //   const results = await Promise.all(
+  //     data.pokemon.map(async ({ pokemon }) => ({
+  //       name: pokemon.name,
+  //       img: null,
+  //       caught: await isPokemonCaught(pokemon.name, req),
+  //     }))
+  //   );
+  //   res.json({ next: null, prev: null, results });
+  // } catch {
+  //   res.status(404).json({ error: "No such pokemon in this collection" });
+  // }
 });
 
 module.exports = type;
